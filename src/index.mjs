cd3d2e1be6eb183a07111e5d4c0c24724dc514fe
rewq455 <ryazap@gmail.com>
2022-02-05 18:06:46 -0500

    Importing spotify-web-api-node. Added config file support.

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,12 @@
-export async function handler(event) {
-    console.log('hello world')
+import spotifyWebApi from 'spotify-web-api-node';
+
+//This is required to import a json file in ES6
+//TODO: Replace this with environment variables for when we are running in the Lambda
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const config = require('./config.json');
+
+export async function handler() {
+    let spotify = new SpotifyWebApi();
+    console.log(config.var);
 }
