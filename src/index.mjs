8dc9f22c232f6bde76cce406a351cffaf1650358
rewq455 <ryazap@gmail.com>
2022-02-05 21:45:34 -0500

    Got a basic spotify query running

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import spotifyWebApi from 'spotify-web-api-node';
+import SpotifyWebApi from 'spotify-web-api-node';
 
 //This is required to import a json file in ES6
 //TODO: Replace this with environment variables for when we are running in the Lambda
@@ -7,6 +7,18 @@ const require = createRequire(import.meta.url);
 const config = require('./config.json');
 
 export async function handler() {
+    let spotify = initialize_spotify();
+    spotify
+        .getMe()
+        .then(response => {
+            console.log(response);
+        });
+}
+
+function initialize_spotify()
+{
     let spotify = new SpotifyWebApi();
-    console.log(config.var);
+    spotify.setAccessToken(config.access_token);
+    spotify.setRefreshToken(config.refresh_token);
+    return spotify;
 }
