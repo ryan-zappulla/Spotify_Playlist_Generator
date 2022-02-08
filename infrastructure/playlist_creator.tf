d5271c1cb4d7f73ead20e10a2189517e1f83d8e3
rewq455 <ryazap@gmail.com>
2022-02-07 21:10:39 -0500

    Role update to give some basic permissions

--- infrastructure/playlist_creator.tf
+++ infrastructure/playlist_creator.tf
@@ -1,27 +1,28 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+data "aws_iam_policy_document" "trust_policy" {
+  statement {
+    actions    = ["sts:AssumeRole"]
+    effect     = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
-  ]
+  }
 }
-EOF
+
+resource "aws_iam_role" "lambda" {
+  name               = var.playlist_creator_lambda_name
+  assume_role_policy = "${data.aws_iam_policy_document.trust_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_policy" {
+  role       = "${aws_iam_role.lambda.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"//TODO: Replace this with a more precise role
 }
 
 resource "aws_lambda_function" "lambda" {
   filename      = "src.zip"
   function_name = var.playlist_creator_lambda_name
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.lambda.arn
   handler       = "index.handler"
   source_code_hash = filebase64sha256("src.zip")//TODO: Get a Code Pipeline in place
   runtime = "nodejs12.x"
