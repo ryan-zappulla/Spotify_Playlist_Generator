25ede975aec855606e2ab4ea51764fef9d6b64ca
rewq455 <ryazap@gmail.com>
2022-02-11 22:46:44 -0500

    Started setting up a promise to be returned from create_playlist

--- src/playlist_creator.ts
+++ src/playlist_creator.ts
@@ -11,30 +11,31 @@ export class Playlist_Creator {
         this.error_handler = error_handler;
     }
 
-    //TODO: Update to return success/failure
-    create_playlist(items: SpotifyApi.PlayHistoryObject[], name: string, isPublic: boolean): void {
-        console.log('create playlist entered')
+    async create_playlist(items: SpotifyApi.PlayHistoryObject[], name: string, isPublic: boolean) : Promise<void>
+    {
         let songs = new Set(items.flatMap(song => `spotify:track:${song.track.id}`)); //Remove duplicates
-        songs.forEach(song => {
-            console.log(song);
+        this.spotify.createPlaylist(name, { 'public': isPublic });
+    
+        return new Promise(function(resolve, reject) {
+            this.spotify.createPlaylist(name, { 'public': isPublic })
+                .then(
+                    (data) => {
+                        let id = data.body.id;
+                        this.spotify.addTracksToPlaylist(id, _.shuffle(Array.from(songs)))
+                            .then(
+                                () => {
+                                    console.log('Created the playlist!');
+                                    resolve();
+                                }, (error) => {
+                                    this.error_handler.handle_error(error);
+                                    reject(error);
+                                }
+                            );
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
         });
-        this.spotify.createPlaylist(name, { 'public': isPublic })
-            .then(
-                (data) => {
-                    let id = data.body.id;
-                    this.spotify.addTracksToPlaylist(id, _.shuffle(Array.from(songs)))
-                        .then(
-                            () => {
-                                console.log('Created the playlist!');
-                            }, (error) => {
-                                this.error_handler.handle_error(error);
-                            }
-                        );
-                },
-                (error) => {
-                    this.error_handler.handle_error(error);
-                }
-            );
     }
-
 }
