68c30fddcc2fd85a55549b514bb353681604fa13
rewq455 <ryazap@gmail.com>
2022-02-11 23:33:53 -0500

    Moved index.ts to using await

--- src/index.ts
+++ src/index.ts
@@ -8,43 +8,26 @@ const config = require('./config.json');
 
 export async function handler(): Promise<void> {
     const error_handler = new Error_Handler();
-    const spotify = await(initialize_spotify(error_handler));
-    console.log('entered')
-    spotify
-        .getMyRecentlyPlayedTracks(
+    try {
+        const spotify = await initialize_spotify();
+        let recentSongs = await spotify.getMyRecentlyPlayedTracks(
             { 
                 limit: 50 //This is the limit, I can get more via pagination if I want
-            }) 
-        .then(
-            (response): void => {
-                console.log('tracks retrieved')
-                let playlist_creator = new Playlist_Creator(spotify, error_handler);//TODO: Look into Nodes naming standards at some point
-                playlist_creator.create_playlist(response.body.items, `Programmed Playlist - ${new Date().toLocaleString()}`, false);
-            },
-            (error) => {
-                error_handler.handle_error(error);
-            }
-        );
+            });
+        let playlist_creator = new Playlist_Creator(spotify, error_handler);
+        await playlist_creator.create_playlist(recentSongs.body.items, `Programmed Playlist - ${new Date().toLocaleString()}`, false);
+    }
+    catch (error) {
+        error_handler.handle_error(error);
+    }
 }
 
-async function initialize_spotify(error_handler : Error_Handler): Promise<SpotifyWebApi>
-{
+async function initialize_spotify(): Promise<SpotifyWebApi> {
     let spotify = new SpotifyWebApi();
     spotify.setRefreshToken(config.refresh_token);
     spotify.setClientId(config.client_id);
     spotify.setClientSecret(config.client_secret);
-
-    return new Promise(function(resolve, reject) {
-        spotify.refreshAccessToken()
-            .then(
-                function(response) {    
-                    spotify.setAccessToken(response.body.access_token);
-                    resolve(spotify);
-                },
-                function(error) {
-                    error_handler.handle_error(error);
-                    reject(error);
-                },
-            );
-    });
+    let refreshResponse = await spotify.refreshAccessToken();
+    await spotify.setAccessToken(refreshResponse.body.access_token);
+    return spotify;
 }
