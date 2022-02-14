c4f34ed513053c90a2626d6704722b4926c764a3
rewq455 <ryazap@gmail.com>
2022-02-13 20:55:22 -0500

    Adding validation around page size, this endpoint does not support pagination

--- src/song_provider.ts
+++ src/song_provider.ts
@@ -9,10 +9,18 @@ export class Song_Provider {
 
     async get_recently_played_songs(count: number) : Promise<SpotifyApi.PlayHistoryObject[]>
     {
+        const maxCount = 50;
+        //https://developer.spotify.com/documentation/web-api/reference/#/operations/get-recently-played
+        //This mentions before and after, however that only seems to work at small page sizes, you cannot go more than 50 songs in the past.
+        if (count > maxCount)
+        {
+            throw new RangeError(`Count needs to be ${maxCount} or less, Spotifies Recent Track endpoint cannot go back more than 50 plays.`);
+        }
         let response = await this.spotify.getMyRecentlyPlayedTracks(
             { 
-                limit: count //50 is the limit, add pagination
-            });
+                limit: count
+            }
+        );
         return response.body.items;
     }
 }
