e08a2b3533a9dded200e8c9453ad119d4b2e31c5
rewq455 <ryazap@gmail.com>
2022-03-08 21:59:58 -0500

    Finished upgrading local_runner to typescript

--- local_runner/index.ts
+++ local_runner/index.ts
@@ -1,11 +1,17 @@
-import { NoopSongLogFacade } from "../src/compiled/song_log_facade.js";
-import { handler, Dependencies } from '../src/compiled/recent_song_logger_index.js';
-import { Spotify_Song_Provider } from '../src/compiled/song_provider.js';
-import { create_spotify } from "../src/compiled/spotify_factory.js";
-import config = require('./config.json');
+import { NoopSongLogFacade } from "../src/song_log_facade";
+import { handler, Dependencies } from '../src/recent_song_logger_index';
+import { Spotify_Song_Provider } from '../src/song_provider';
+import { create_spotify } from "../src/spotify_factory";
 
-// let spotify = await create_spotify(config.client_id, config.client_secret, config.refresh_token);
-// let dependencies = new Dependencies();
-// dependencies.song_provider = new Spotify_Song_Provider(spotify)
-// dependencies.song_log_facade = new NoopSongLogFacade("2022-02-26T22:45:33.475Z");
-// await handler(dependencies);
+declare function require(name:string);
+const config = require("./config.json");
+
+async function run() {
+    let spotify = await create_spotify(config.client_id, config.client_secret, config.refresh_token);
+    let dependencies = new Dependencies();
+    dependencies.song_provider = new Spotify_Song_Provider(spotify)
+    dependencies.song_log_facade = new NoopSongLogFacade("2022-02-26T22:45:33.475Z");
+    await handler(dependencies);
+}
+
+run();
